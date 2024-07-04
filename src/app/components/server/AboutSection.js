b3341f865fc1d8d45c63b47b1fa16974764cba42
Manav1011 <manavshah1011.ms@gmail.com>
2024-07-05 01:01:08 +0530

    About section Height changed

--- src/app/components/server/AboutSection.js
+++ src/app/components/server/AboutSection.js
@@ -10,7 +10,7 @@ async function AboutSection() {
             ABOUT
           </span>
           <span
-            className="hover:text-black ease-in-out duration-500 text-slate-200 text-xs xss:text-sm sm:text-2xl md:text-3xl lg:text-sm"
+            className="hover:text-black ease-in-out duration-500 text-slate-200 text-xs xss:text-sm sm:text-2xl md:text-3xl lg:text-xl"
             style={{ fontFamily: "BebasNeue" }}
           >
             A Dedicated full-stack developer specializing in combining front-end
